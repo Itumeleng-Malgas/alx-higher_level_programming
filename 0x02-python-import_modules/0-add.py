f003f10717e14bc2f1b45f78d3135241d10c5e60
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-11 07:49:01 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
 a = 1
 b = 2
-print("<a {}> + <b {}> = <add(a, b) {}>".format(a,b,add(a,b)))
+
+print(f"<a {a}> + <b {b}> = <add(a, b) {add(a,b)}>")
