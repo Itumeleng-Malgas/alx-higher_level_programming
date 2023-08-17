335b7bc2137803543f90cc298c2e7361c8ec0c3c
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-17 09:49:39 +0200

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_values = set()
+    return sum(num for num in my_list
+               """ If the num is not already in the set, it will be added """
+               if num not in unique_values and not unique_values.add(num))
