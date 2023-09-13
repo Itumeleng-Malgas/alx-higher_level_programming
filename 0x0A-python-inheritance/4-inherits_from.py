3ddb5270e14f8a49629b80e4dae37b91d34f4739
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-13 08:54:48 +0200

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""checks if a given object is only sub class of a specified class"""
+
+
+def inherits_from(obj, a_class):
+    """
+    check if obj is a sub class of a_class
+
+    Args:
+        obj (any):      The given object
+        a_class (type): The class to check against
+
+    Returns: True, if obj is inherited from a_class, otherwise false
+    """
+
+    if issubclass(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
