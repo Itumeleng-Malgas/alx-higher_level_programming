81cf3c9db32a3fae704ed7684428698c4933dbff
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-13 22:01:39 +0200

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines a Square, a BaseGeometry method"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """implements area a method of BaseGeometry super class"""
+
+    def __init__(self, size):
+        """Initializes a Square an instance with size"""
+        self.integer_validator("size", size)
+        self._size = size
+
+    def area(self):
+        """compute the area with given width and height"""
+        return self._size * self._size
+
+    def __str__(self):
+        """Returns a string representation of the Square"""
+        return "[Square] {}/{}".format(self._size, self._size)
