0324bae316f7ba121c9f08a0477219e7ce144b60
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-14 01:46:14 +0200

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that appends a string to a text file (UTF8)"""
+
+
+def write_file(filename="", text=""):
+    """appends contents to a file
+    Args:
+        filename (str): file name to write to
+        text (str):     contents to be appended
+    Returns: The number of chars addded
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
