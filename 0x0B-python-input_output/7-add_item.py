c06ed139c95c560b5de9f6dc0c76e26b6b2c2f86
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-14 02:48:40 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ import sys
 
 
 def load_add_save():
+    """adds all arguments to a Python list, and then save them to a file"""
     save_to_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_file = __import__("6-load_from_json_file").load_from_json_file
 
@@ -17,6 +18,7 @@ def load_add_save():
 
     items.extend(sys.argv[1:])
     save_to_file(items, "add_item.json")
+
 
 if __name__ == "__main__":
     load_add_save()
