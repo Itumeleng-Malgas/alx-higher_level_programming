9436cf19424bcb2c80c28e82f20ecf2fbb1ba5f2
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-14 03:25:49 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a class Student"""
+
+
+class Student:
+    """represent a student obj"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a student obj"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        self.__dict__
