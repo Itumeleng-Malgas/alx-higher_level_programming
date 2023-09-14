fba55c522812da7a8aa457f3525f714d7506f01c
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-14 03:27:19 +0200

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,13 @@ class Student:
     """represent a student obj"""
 
     def __init__(self, first_name, last_name, age):
-        """initialize a student obj"""
+        """initialize a student obj
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
