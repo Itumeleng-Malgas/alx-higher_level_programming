9548ac17db6bc8301f8a7d917009e6d6852dc68d
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-21 13:11:09 +0200

    square_impl

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+""" implements the square, as a sub class of rectangle """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ square class implementation """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initialize the square class """
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """ prints a formatted view of a square """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
