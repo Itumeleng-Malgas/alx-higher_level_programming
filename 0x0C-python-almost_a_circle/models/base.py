d27b185ccbca2d56aa576d6e212f460b3bfc609a
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-22 03:00:36 +0200

    create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,24 @@ class Base:
             else:
                 list_dictionaries = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string """
+        if json_string is None or json_string == "[]":
+            return []
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """  returns an instance with all attributes already set """
+        if dictionary and len(dictionary) != 0:
+            if cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            else:
+                dummy = cls(1)
+
+            dummy.update(**dictionary)
+
+            return dummy
