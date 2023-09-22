62cec6e9e49b9d30c88701eeb3cacb2da4090cfc
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-09-22 02:50:15 +0200

    save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,3 +37,15 @@ class Base:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ saves list object to JSON file """
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(list_dictionaries))
