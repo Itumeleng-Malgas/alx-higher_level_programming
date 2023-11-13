7046b87e271bcabe6d9297b3263735e535927c73
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-13 19:20:42 +0200

    2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-
-let multi_lang = 'C is fun\nPython is console\nJavaScript is amazing';
-console.log(multi_lang);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
