68e0b1ddae9ef59ae6bef2b5b08b02f452b75c58
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-14 22:59:03 +0200

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (this.isValidDimension(w) && this.isValidDimension(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  isValidDimension (value) {
+    return Number.isInteger(value) && value > 0;
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
