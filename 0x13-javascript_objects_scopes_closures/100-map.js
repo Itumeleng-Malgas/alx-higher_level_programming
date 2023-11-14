5219f06b990ac0acb3ade981a3d65661feb247b0
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-14 23:31:08 +0200

    advanced_tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+
+const dict = require('./100-data.js').list;
+
+console.log(dict);
+console.log(dict.map((item, index) => item * index));
