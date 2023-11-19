663ab187311925314ac01c93a660cf200e069451
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-19 18:56:02 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -32,10 +32,6 @@ def list_states(username, password, database_name):
 
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        sys.exit(1)
-
     # Get username, password, and database name from command line arguments
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
     list_states(username, password, database_name)
