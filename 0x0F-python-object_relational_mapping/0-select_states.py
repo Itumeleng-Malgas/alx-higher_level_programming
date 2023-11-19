29c7836f6936c30a9aa2af6879c37038c2e84ee2
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-19 18:43:50 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def list_states(username, password, database_name):
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the query to retrieve states in ascending order by id
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get MySQL username, password, and database name from command line arguments
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, database_name)
+
