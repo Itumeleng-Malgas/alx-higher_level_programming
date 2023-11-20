968c5dbef9c5db28938a7e843407375fb873e29a
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-20 18:59:37 +0200

    0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,11 +4,8 @@ import sys
 
 
 if __name__ == "__main__":
-
-    # Get username, password, and database name from command line arguments
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,14 +15,10 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-
-    # Execute the query to retrieve states in ascending order by id
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
 
-    # Fetch all the rows
     states = cursor.fetchall()
-
     for state in states:
         print(state)
 
