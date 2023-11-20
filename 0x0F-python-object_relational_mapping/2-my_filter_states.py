1a5e15af500975abc4a95fb6b61103412b2e80eb
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-20 20:17:28 +0200

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name LIKE BINARY
+    '{}'".format(sys.argv[4])
     cursor.execute(query)
 
     states = cursor.fetchall()
