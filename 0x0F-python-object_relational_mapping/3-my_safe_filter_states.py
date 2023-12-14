99d4466d6711e157b8e42bbdfd76713ec8e3f142
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-14 18:32:39 +0200

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" lists all states from hbtn_0e_0_usa db  starting with N """
+"""Lists all states where name matches the argv save sqlinjection"""
+
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     db = MySQLdb.connect(
-        host="127.0.0.0",
+        host="127.0.0.1",
         port=3306,
         user=username,
         passwd=password,
@@ -16,9 +16,12 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = """SELECT * FROM states WHERE name LIKE BINARY 'N%'
-    ORDER BY states.id"""
-    cursor.execute(query)
+
+    # Use parameterized query to prevent SQL injection
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    name_param = (sys.argv[4],)
+
+    cursor.execute(query, name_param)
 
     states = cursor.fetchall()
     for state in states:
