d6660a318367c35d60f5a69dab707e601e7b6ea0
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 18:45:15 +0200

    model_state.py:19:1: W293 blank line contains whitespace

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
     Defines states columns, attributes of each state
