afdd62612b0fef6b687188cd46c6ad30cc8a50ea
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 18:30:09 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 lists all State objects from the database hbtn_0e_6_usa
 """
 
-from model_state import State
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 import sys
