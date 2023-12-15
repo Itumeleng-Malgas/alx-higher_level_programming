34f3dcef67af862686d1a88e3e03ad9590431ef9
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 18:42:45 +0200

    6-model_state.py:14:37: W292 no newline at end of file

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""
+Start link class to table in database
 """
 from sqlalchemy import (create_engine)
 from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
-    database_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3])
+    database_url = 'mysql+mysqldb://{}:{}@localhost/{}' \
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
 
     engine = create_engine(database_url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
