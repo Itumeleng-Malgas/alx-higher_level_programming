44aaf3e81e3f0fb743a206a8afccdfa9619b771a
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 19:23:03 +0200

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter a
+"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    database_url = 'mysql+mysqldb://{}:{}@localhost/{}' \
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    engine = create_engine(database_url, pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
