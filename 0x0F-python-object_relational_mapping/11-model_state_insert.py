72b644b4afed97e381d6b39d80c7412972fe6a10
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-15 19:32:21 +0200

    11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter a
+"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    database_url = 'mysql+mysqldb://{}:{}@localhost/{}' \
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    engine = create_engine(database_url, pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
