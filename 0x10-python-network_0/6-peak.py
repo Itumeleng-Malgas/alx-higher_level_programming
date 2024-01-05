fb05c60eee208c24941cc4eb39aa0bd625ec7460
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-06 01:44:23 +0200

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,19 @@
 
 def find_peak(list_of_integers):
     """ find peak impl """
+    if not list_of_integers:
+        return None
+
     low, high = 0, len(list_of_integers) - 1
 
     while low < high:
         mid = (low + high) // 2
 
-        # Move towards the direction of a higher neighbor
-        high = (mid if list_of_integers[mid] > list_of_integers[mid + 1]
-                else mid + 1)
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # Move towards the direction of a higher neighbor on the left
+            high = mid
+        else:
+            # Move towards the direction of a higher neighbor on the right
+            low = mid + 1
 
     return list_of_integers[low]
