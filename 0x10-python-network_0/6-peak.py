5f6c8b4be9880021b60b2f7d8e9ed51e7cfc3c8c
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-06 01:22:52 +0200

    6-peak.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" find peak mod """
+
+def find_peak(list_of_integers):
+    """ find peak impl """
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        # Move towards the direction of a higher neighbor
+        high = (mid if list_of_integers[mid] > list_of_integers[mid + 1]
+                else mid + 1)
+
+    return list_of_integers[low]
