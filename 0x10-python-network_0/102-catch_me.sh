de1f4bc8b2c8a6e9c5f332bd0d2cdeb93b637f55
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-06 02:20:57 +0200

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me and cause the server to respond with "You got me!"
-curl -s -X PUT -L -d "user_id=1337" "0.0.0.0:5000/catch_me" > /dev/null 2>&1
+curl -s -LX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
