7357efba4e9f4087bd97860ed8af25c9e3a61612
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-07 16:37:20 +0200

    Function name fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 
-def get_commit(repo_name, owner_name):
+def get_commits(repo_name, owner_name):
     """
     Calls github api to retrieve commits.
     """
@@ -24,9 +24,5 @@ def get_commit(repo_name, owner_name):
         print(f"{sha}: {author_name}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <repository_name> <owner_name>")
-        sys.exit(1)
-
-        repo_name, owner_name = sys.argv[1], sys.argv[2]
-        get_commits(repo_name, owner_name)
+    repo_name, owner_name = sys.argv[1], sys.argv[2]
+    get_commits(repo_name, owner_name)
