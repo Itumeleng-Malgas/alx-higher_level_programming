bb27c8070aad08b03b2d6ba5c994c62a9b2c11fa
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-07 10:55:11 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" """
+import urllib.request
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        content = resp.read()
         utf8_content = content.decode('utf-8')
 
-    print(f"Body response:\n"
-          f"    - type: {type(content)}\n"
-          f"    - content: {content}\n"
-          f"    - utf8 content: {utf8_content}")
+    print("Body response:")
+    print("    - type:", type(content))
+    print("    - content:", content)
+    print("    - utf8 content:", utf8_content)
