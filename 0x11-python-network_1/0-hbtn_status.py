03d431eb52d3ead406eb768808d11feb676f8961
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-07 11:39:55 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+A simple script to make an HTTP request to https://alx-intranet.hbtn.io/status
+and print information about the response.
+"""
 import urllib.request
 
 if __name__ == "__main__":
+    # Open the specified URL and read the response
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
         utf8_content = content.decode('utf-8')
 
-    print("Body response:")
-    print("    - type:", type(content))
-    print("    - content:", content)
-    print("    - utf8 content:", utf8_content)
+        # Print information about the response
+        print("Body response:")
+        print("    - type:", type(content))
+        print("    - content:", content)
+        print("    - utf8 content:", utf8_content)
