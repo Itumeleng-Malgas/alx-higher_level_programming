6bfb1a02e3fcb83143a16d0982e6a84444fb419d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-07 10:48:43 +0200

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+""" """
 
-if __name__ == "__main__":
+
+def main():
+    """ Fetches https://alx-intranet.hbtn.io/status """
+
+    url = 'https://alx-intranet.hbtn.io/status'
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
 
-    print(
-            f"Body response:\n"
-            f"    - type: {type(content)}\n"
-            f"    - content: {content}\n"
-            f"    - utf8 content: {utf8_content}"
-            )
+    print(f"Body response:\n"
+          f"    - type: {type(content)}\n"
+          f"    - content: {content}\n"
+          f"    - utf8 content: {utf8_content}")
+
+
+if __name__ == "__main__":
+    main()
