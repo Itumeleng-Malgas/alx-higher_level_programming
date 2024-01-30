ce0ca9404878dcd103fde1ecfaf14036de2b0975
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-30 10:25:02 +0200

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+if (process.argv.length !== 3)
+{
+	console.error('Usage: node 0-readme.js <file-path>');
+	process.exit(1);
+}
+
+function handleFileRead(err, data)
+{
+	if (!err) {
+		console.log(data);
+	}
+}
+
+const filePath = process.argv[2]
+fs.readFile(filePath, 'utf-8', handleFileRead);
