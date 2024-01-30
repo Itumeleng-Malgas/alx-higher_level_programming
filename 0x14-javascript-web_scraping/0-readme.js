cff02547bee54b8487097b60760f2fab9122c048
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-30 10:33:49 +0200

    ran semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-if (process.argv.length !== 3)
-{
-	console.error('Usage: node 0-readme.js <file-path>');
-	process.exit(1);
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1);
 }
 
-function handleFileRead(err, data)
-{
-	if (!err) {
-		console.log(data);
-	}
+function handleFileRead (err, data) {
+  if (!err) {
+    console.log(data);
+  }
 }
 
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', handleFileRead);
