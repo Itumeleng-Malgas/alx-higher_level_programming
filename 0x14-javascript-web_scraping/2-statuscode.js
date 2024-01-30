d3b4b29f0a7a60c9c2711d830595f46d427b07f2
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-30 11:04:10 +0200

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./2-statuscode.js <url>');
+  process.exit(1);
+}
+const url = process.argv[2];
+
+function handleRequestCallback(err, res) {
+  if (!err) {
+    console.log(`code: ${res.statusCode}`);
+  } else {
+    console.log(err)
+  }
+}
+
+request(url, handleRequestCallback);
